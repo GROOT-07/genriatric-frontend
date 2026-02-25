d864947cefd33925ef7f6b2368fd2807502ea6fd
GROOT-07 <manishankar.katuri07@gmail.com>
2026-02-25 12:30:48 +0530

    sync fix

--- src/SlotBooking.js
+++ src/SlotBooking.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const API = process.env.REACT_APP_API_URL;
+console.log("API BASE URL:", API);
 
 function SlotBooking() {
   const [date, setDate] = useState("");
